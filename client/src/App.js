419ede1aca8ce7be3fbd6b5f851ec7eaedd68a54
Pratham <prathamsharmafaangm@gmail.com>
2022-10-19 00:55:44 +0530

    Initial structure added

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
+
+const App = () => {
+  
+  return (
+    <div >
+      <div className="bg-gray-900 text-gray-200 min-h-screen ">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b border-gray-700 '>Navbar</div>
+  )
+}
+
+export default Navbar
